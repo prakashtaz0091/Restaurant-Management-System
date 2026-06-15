10063c6c27dc3dfe388db3bc4ad24f53418364e3
prakashtaz0091 <code.ing2468@gmail.com>
2026-06-15 20:52:52 +0545

    feat: custom roles required decorator for resource authorization

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .decorators import role_required
+from accounts.models import User
 
-@login_required
+
+@role_required([User.ROLE_CHOICES.WAITER])
 def tables_view(request):
     return render(request, "orders/tables.html")
