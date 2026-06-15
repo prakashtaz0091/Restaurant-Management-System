fba2add79a74fdde6117384c195a7e3759267425
prakashtaz0091 <code.ing2468@gmail.com>
2026-06-15 21:29:25 +0545

    feat: management command for configuring tables in a restaurant

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Table
 
-# Register your models here.
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ["name", "is_reserved"]
