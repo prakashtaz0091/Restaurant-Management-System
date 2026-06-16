2bc6ff49faa3722365c555a5d24cf608100969e3
prakashtaz0091 <code.ing2468@gmail.com>
2026-06-16 20:42:00 +0545

    feat: management command to setup new restaurant's menu

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Table
+from .models import Table, Category, MenuItem
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
     list_display = ["name", "is_reserved"]
+    
+
+admin.site.register(Category)
+admin.site.register(MenuItem)
--- orders/models.py
+++ orders/models.py
@@ -7,3 +7,23 @@ class Table(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
+    
+    
+class MenuItem(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="items")
+    name = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+    description = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
