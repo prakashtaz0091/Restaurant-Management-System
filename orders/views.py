40a242600153d947d061f6aa36073705715ed2a2
prakashtaz0091 <code.ing2468@gmail.com>
2026-06-16 22:10:31 +0545

    menu display

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .decorators import role_required
 from accounts.models import User
-from .models import Table
+from .models import Table, Category
 
 
 @role_required([User.ROLE_CHOICES.WAITER])
@@ -10,4 +10,14 @@ def tables_view(request):
     
     return render(request, "orders/tables.html", {
         'tables':tables
+    })
+    
+    
+@role_required([User.ROLE_CHOICES.WAITER])
+def menu_view(request, table_id):
+    categories = Category.objects.all()
+    
+    return render(request, "orders/menu.html", {
+        'categories':categories,
+        'table_id':table_id
     })
