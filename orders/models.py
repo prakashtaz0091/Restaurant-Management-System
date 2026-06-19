4a11b5e41dc5c6bcb35c3629d7890217215db33b
prakashtaz0091 <code.ing2468@gmail.com>
2026-06-19 20:31:36 +0545

    feat: individual order item status tracking and auto order status update

--- orders/models.py
+++ orders/models.py
@@ -32,14 +32,14 @@ class MenuItem(models.Model):
 
 class Order(models.Model):
     class ORDER_STATUS(models.TextChoices):
-        PREPARING = 'p', 'Preparing'
-        READY = 'r', 'Ready'
+        PENDING = 'p', 'Pending'
+        PARTIALLY_SERVED = 'ps', 'Partially Served'
         SERVED = 's', 'Served'
         BILLED = 'b', 'Billed'
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     table = models.ForeignKey(Table, on_delete=models.PROTECT, related_name="orders")
-    status = models.CharField(max_length=2, choices=ORDER_STATUS, default=ORDER_STATUS.PREPARING)
+    status = models.CharField(max_length=2, choices=ORDER_STATUS, default=ORDER_STATUS.PENDING)
     
     created_at = models.DateTimeField(auto_now_add=True)
     
@@ -48,10 +48,16 @@ class Order(models.Model):
     
     
 class OrderItem(models.Model):
+    class ITEM_STATUS(models.TextChoices):
+        PREPARING = 'p', 'Preparing'
+        READY = 'r', 'Ready'
+        SERVED = 's', 'Served'
+
     order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name="items")
     menu_item = models.ForeignKey(MenuItem, on_delete=models.PROTECT, related_name="order_items")
     price = models.PositiveIntegerField()
     quantity = models.PositiveIntegerField(default=1)
+    status = models.CharField(choices=ITEM_STATUS, max_length=2, default=ITEM_STATUS.PREPARING)
     
     def __str__(self):
         return f"{self.menu_item}  x {self.quantity} qtys"
