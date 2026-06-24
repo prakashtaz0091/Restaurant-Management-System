c800e949220b38622d8704a5df82840d0b1b18b5
prakashtaz0091 <code.ing2468@gmail.com>
2026-06-24 21:39:09 +0545

    feat: billing - on going

--- orders/views.py
+++ orders/views.py
@@ -210,3 +210,7 @@ def billing_tables_status_live(request):
         'tables':list(set(tables))
     })
     
+    
+@role_required([User.ROLE_CHOICES.BILLING])
+def billing_view(request, table_id):
+    return render(request, "orders/billing.html")
